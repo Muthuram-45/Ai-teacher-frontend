8f522afb0d30f8f85a2f846cf15dee7d7fbc180c
Muthuram-45 <muthuram921@gmail.com>
2026-05-15 21:32:06 +0530

    url

--- app/lib/config.js
+++ app/lib/config.js
@@ -1,3 +1,3 @@
-export const BACKEND_URL = process.env.NEXT_PUBLIC_BACKEND_URL || 'http://localhost:3001';
+// export const BACKEND_URL = process.env.NEXT_PUBLIC_BACKEND_URL || 'http://localhost:3001';
 export const PYTHON_BACKEND_URL = process.env.NEXT_PUBLIC_PYTHON_BACKEND_URL || 'http://localhost:5000';
-// export const BACKEND_URL = process.env.NEXT_PUBLIC_BACKEND_URL || 'https://ai-teacher-backend-r9pc.onrender.com';
+export const BACKEND_URL = process.env.NEXT_PUBLIC_BACKEND_URL || 'https://ai-teacher-backend-r9pc.onrender.com';
