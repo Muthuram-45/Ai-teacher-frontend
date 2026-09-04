7cc1e7f55e365d8346c2c7fb5daace98e09242f4
Muthuram-45 <muthuram921@gmail.com>
2026-09-04 16:03:23 +0530

    VIDEOGEN_URL live url

--- app/lib/config.js
+++ app/lib/config.js
@@ -1,8 +1,9 @@
 
 //export const BACKEND_URL = process.env.NEXT_PUBLIC_BACKEND_URL || 'http://localhost:3001';
 export const BACKEND_URL = process.env.NEXT_PUBLIC_BACKEND_URL || 'https://ai-teacher-backend-r9pc.onrender.com';
-export const PYTHON_BACKEND_URL = process.env.NEXT_PUBLIC_PYTHON_BACKEND_URL || 'http://localhost:5000';
-export const VIDEOGEN_URL = process.env.NEXT_PUBLIC_VIDEOGEN_URL || 'http://localhost:5000';
+// export const PYTHON_BACKEND_URL = process.env.NEXT_PUBLIC_PYTHON_BACKEND_URL || 'http://localhost:5000';
+//export const VIDEOGEN_URL = process.env.NEXT_PUBLIC_VIDEOGEN_URL || 'http://localhost:5000';
+export const VIDEOGEN_URL = process.env.NEXT_PUBLIC_VIDEOGEN_URL || 'https://videogenerator-backend-ul5i.onrender.com';
 export const ATTENTION_POPUP_INTERVAL = process.env.NEXT_PUBLIC_ATTENTION_POPUP_INTERVAL || '10';
 
 export const INACTIVITY_THRESHOLD_MS = 2 * 60 * 1000; // 2 minutes
